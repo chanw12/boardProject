3cf4ce6ff1fdc03090f8840fa16b9091c5ce6860
chanw12 <wooju9812127@gmail.com>
2023-02-14 16:34:58 +0900

    게시판 list페이지 화면작성

--- src/main/java/board/boardproject/web/HelloController.java
+++ src/main/java/board/boardproject/web/HelloController.java
@@ -1,13 +1,15 @@
 package board.boardproject.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "hello";
+    @GetMapping("/board/list")
+    public String hello(Model model){
+        return "board/list";
     }
 }
